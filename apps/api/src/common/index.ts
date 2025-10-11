6843bc6cf50f55a111ae46d8e6df2a513987f73d
shailendra <shailendramourya17@gmail.com>
2025-10-11 14:09:36 +0530

    add server service

--- apps/api/src/common/index.ts
+++ apps/api/src/common/index.ts
@@ -5,3 +5,5 @@ export * from './guards/roles.guard';
 export * from './decorators/roles.decorator';
 export * from './prisma/prisma.service';
 export * from './prisma/prisma.module';
+export * from './repositories';
+export * from './database';
